637d0ef6ae58e11e07a132b596b72f9e6e29faa8
Edip YILDIZ <edip.yildiz@hotmail.com>
2017-06-12 14:49:24 +0300

    bugfix

--- lider-console-core/src/tr/org/liderahenk/liderconsole/core/ldap/utils/LiderLdapConnection.java
+++ lider-console-core/src/tr/org/liderahenk/liderconsole/core/ldap/utils/LiderLdapConnection.java
@@ -2,8 +2,8 @@ package tr.org.liderahenk.liderconsole.core.ldap.utils;
 
 import java.util.List;
 
-import org.springframework.ldap.core.LdapTemplate;
-import org.springframework.ldap.core.support.LdapContextSource;
+//import org.springframework.ldap.core.LdapTemplate;
+//import org.springframework.ldap.core.support.LdapContextSource;
 
 public class LiderLdapConnection {
 	
@@ -12,26 +12,27 @@ public class LiderLdapConnection {
 	public List<String> getTemplate(String url, String baseDn, String userName, String password){
 		
 		
-	    LdapContextSource contextSource = new LdapContextSource();
-	    contextSource.setUrl("ldap://"+url+":389");
-	    contextSource.setBase(baseDn);
-	    contextSource.setUserDn("cn="+userName+","+baseDn);
-	    contextSource.setPassword(password);
-	    
-
-	    try {
-	        contextSource.afterPropertiesSet();
-	    } catch (Exception ex) {
-	        ex.printStackTrace();
-	    }
-
-
-	    LdapTemplate template = new LdapTemplate();
-
-	    template.setContextSource(contextSource);
-	    List<String> children = template.list("");
-	    
-	    return children;
+//	    LdapContextSource contextSource = new LdapContextSource();
+//	    contextSource.setUrl("ldap://"+url+":389");
+//	    contextSource.setBase(baseDn);
+//	    contextSource.setUserDn("cn="+userName+","+baseDn);
+//	    contextSource.setPassword(password);
+//	    
+//
+//	    try {
+//	        contextSource.afterPropertiesSet();
+//	    } catch (Exception ex) {
+//	        ex.printStackTrace();
+//	    }
+//
+//
+//	    LdapTemplate template = new LdapTemplate();
+//
+//	    template.setContextSource(contextSource);
+//	    List<String> children = template.list("");
+//	    
+//	    return children;
+		return null;
 
 	}
 }
