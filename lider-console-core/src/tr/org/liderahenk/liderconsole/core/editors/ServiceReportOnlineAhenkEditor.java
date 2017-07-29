107f9f9618d60b96e461e043aff2320fe72f88ef
Edip YILDIZ <edip.yildiz@hotmail.com>
2017-07-29 11:18:22 +0300

    online aheknlerin servisleri listelenip filtrelenmiştir.

--- lider-console-core/src/tr/org/liderahenk/liderconsole/core/editors/ServiceReportOnlineAhenkEditor.java
+++ lider-console-core/src/tr/org/liderahenk/liderconsole/core/editors/ServiceReportOnlineAhenkEditor.java
@@ -32,6 +32,7 @@ import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.ProgressBar;
 import org.eclipse.swt.widgets.TabFolder;
 import org.eclipse.swt.widgets.TabItem;
 import org.eclipse.swt.widgets.Table;
@@ -53,10 +54,8 @@ import tr.org.liderahenk.liderconsole.core.ldap.enums.DNType;
 import tr.org.liderahenk.liderconsole.core.model.Agent;
 import tr.org.liderahenk.liderconsole.core.model.AgentServiceListItem;
 import tr.org.liderahenk.liderconsole.core.model.AgentServices;
-import tr.org.liderahenk.liderconsole.core.model.MailAddress;
 import tr.org.liderahenk.liderconsole.core.rest.requests.TaskRequest;
 import tr.org.liderahenk.liderconsole.core.rest.utils.TaskRestUtils;
-import tr.org.liderahenk.liderconsole.core.utils.SWTResourceManager;
 import tr.org.liderahenk.liderconsole.core.widgets.Notifier;
 import tr.org.liderahenk.liderconsole.core.xmpp.XMPPClient;
 import tr.org.liderahenk.liderconsole.core.xmpp.notifications.TaskStatusNotification;
@@ -69,13 +68,9 @@ public class ServiceReportOnlineAhenkEditor  extends EditorPart{
 	
 	private TableViewer tableViewerService;
 	private Label labelRecordDate;
-	// private List<AgentService> list = new ArrayList<AgentService>();
-	private String agentDn;
 	private Table tableServiceList;
 	private Text textServiceName;
 	private Text textAgent;
-	private TableColumn columnAhenk;
-	private TableColumn columnServices;
 	private Composite composite;
 	private Composite mainPanel;
 	private TabFolder tabFolder;
@@ -86,7 +81,9 @@ public class ServiceReportOnlineAhenkEditor  extends EditorPart{
 	public ServiceReportOnlineAhenkEditor() {
 	}
 
-	private DefaultEditorInput editorInput;
+	private DefaultEditorInput editorInput=null;
+
+	private ProgressBar progressBar;
 
 	@Override
 	public void doSave(IProgressMonitor monitor) {
@@ -106,7 +103,6 @@ public class ServiceReportOnlineAhenkEditor  extends EditorPart{
 		setInput(input);
 		editorInput = (DefaultEditorInput) input;
 		
-	
 		//eventBroker.subscribe(LiderConstants.EVENT_TOPICS.TASK_STATUS_NOTIFICATION_RECEIVED, eventHandler);
 		
 		eventBroker.subscribe("service".toUpperCase(Locale.ENGLISH), eventHandler);
@@ -116,13 +112,11 @@ public class ServiceReportOnlineAhenkEditor  extends EditorPart{
 
 	@Override
 	public boolean isDirty() {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public boolean isSaveAsAllowed() {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
@@ -141,12 +135,23 @@ public class ServiceReportOnlineAhenkEditor  extends EditorPart{
 
 	Group grpOnlineUsr = new Group(tabFolder, SWT.BORDER | SWT.SHADOW_IN);
 	tbtmCreateFile.setControl(grpOnlineUsr);
-	grpOnlineUsr.setLayout(new GridLayout(4, false));
+	grpOnlineUsr.setLayout(new GridLayout(5, false));
 
 	SimpleDateFormat format = new SimpleDateFormat("dd.MM.yyyy HH:mm:ss");
 	labelRecordDate = new Label(grpOnlineUsr, SWT.NONE);
 	labelRecordDate.setText(Messages.getString("DATE") + " " + format.format(new Date()));
 	new Label(grpOnlineUsr, SWT.NONE);
+	
+	btnRefresh = new Button(grpOnlineUsr, SWT.NONE);
+	btnRefresh.addSelectionListener(new SelectionAdapter() {
+		@Override
+		public void widgetSelected(SelectionEvent e) {
+			
+			getServices();
+		}
+	});
+	btnRefresh.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false, false, 1, 1));
+	btnRefresh.setText(Messages.getString("LiderManagementEditor.btnRefreshPolicy.toolTipText"));
 	new Label(grpOnlineUsr, SWT.NONE);
 	new Label(grpOnlineUsr, SWT.NONE);
 
@@ -160,6 +165,7 @@ public class ServiceReportOnlineAhenkEditor  extends EditorPart{
 	textAgent.setLayoutData(gd_textAgent);
 	new Label(grpOnlineUsr, SWT.NONE);
 	new Label(grpOnlineUsr, SWT.NONE);
+	new Label(grpOnlineUsr, SWT.NONE);
 
 	Label label = new Label(grpOnlineUsr, SWT.NONE);
 	label.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
@@ -169,6 +175,7 @@ public class ServiceReportOnlineAhenkEditor  extends EditorPart{
 	GridData gd_textPackageName = new GridData(SWT.LEFT, SWT.CENTER, false, false, 1, 1);
 	gd_textPackageName.widthHint = 324;
 	textServiceName.setLayoutData(gd_textPackageName);
+	new Label(grpOnlineUsr, SWT.NONE);
 	new Label(grpOnlineUsr, SWT.NONE);
 	new Label(grpOnlineUsr, SWT.NONE);
 
@@ -193,36 +200,45 @@ public class ServiceReportOnlineAhenkEditor  extends EditorPart{
 			String serviceName = textServiceName.getText();
 			String status = combo.getText();
 
-			status = status.equals("Started") ? "started" : status.equals("Stopped") ? "stopped" : "";
-			// filter(agent, serviceName, status);
+			status = status.equals("Started") ? "ACTIVE" : status.equals("Stopped") ? "INACTIVE" : "";
+			filter(agent, serviceName, status);
 
 		}
 	});
 	btnFilter.setText(Messages.getString("filter"));
+	new Label(grpOnlineUsr, SWT.NONE);
+	new Label(grpOnlineUsr, SWT.NONE);
+	
+	progressBar = new ProgressBar(grpOnlineUsr, SWT.SMOOTH | SWT.INDETERMINATE);
+	progressBar.setSelection(0);
+	progressBar.setMaximum(100);
+	GridData gdProgress = new GridData(GridData.FILL_HORIZONTAL);
+	gdProgress.horizontalSpan = 3;
+	gdProgress.heightHint = 10;
+	progressBar.setLayoutData(gdProgress);
+	progressBar.setVisible(true);
+	new Label(grpOnlineUsr, SWT.NONE);
+	new Label(grpOnlineUsr, SWT.NONE);
 
-	Button btnExportToPDF = new Button(grpOnlineUsr, SWT.NONE);
-	btnExportToPDF.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, true, false, 1, 1));
-	btnExportToPDF.addSelectionListener(new SelectionAdapter() {
-		@Override
-		public void widgetSelected(SelectionEvent e) {
-			// exportToPdf();
-		}
-	});
-	btnExportToPDF.setText(Messages.getString("EXPORT"));
+//	Button btnExportToPDF = new Button(grpOnlineUsr, SWT.NONE);
+//	btnExportToPDF.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, true, false, 1, 1));
+//	btnExportToPDF.addSelectionListener(new SelectionAdapter() {
+//		@Override
+//		public void widgetSelected(SelectionEvent e) {
+//			 exportToPdf();
+//		}
+//	});
+//	btnExportToPDF.setText(Messages.getString("EXPORT"));
 
 	composite = new Composite(grpOnlineUsr, SWT.BORDER | SWT.SHADOW_IN);
 	composite.setLayout(new GridLayout(1, false));
-	composite.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 4, 2));
+	composite.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 5, 2));
 
 	tableViewerService = new TableViewer(composite,
 			SWT.MULTI | SWT.H_SCROLL | SWT.V_SCROLL | SWT.FULL_SELECTION | SWT.BORDER);
 
 	// createTableColumns();
 	tableServiceList = tableViewerService.getTable();
-
-	// list = getList();
-
-	// table = new Table(composite, SWT.BORDER | SWT.MULTI);
 
 	tableServiceList.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 2));
 	tableServiceList.setHeaderVisible(true);
@@ -234,15 +250,12 @@ public class ServiceReportOnlineAhenkEditor  extends EditorPart{
 	tableViewerService.getTable().pack();
 	
 	createServiceTableColumns();
-
 	getServices();
 	
 	}
-	
-	
 
 	private void createServiceTableColumns() {
-		TableViewerColumn agentDnColumn = SWTResourceManager.createTableViewerColumn(tableViewerService,	Messages.getString("mail_address"), 200);
+		TableViewerColumn agentDnColumn = createTableViewerColumn("Agent",150);
 		agentDnColumn.getColumn().setAlignment(SWT.LEFT);
 		agentDnColumn.setLabelProvider(new ColumnLabelProvider() {
 			@Override
@@ -253,7 +266,7 @@ public class ServiceReportOnlineAhenkEditor  extends EditorPart{
 				return null;
 			}
 		});
-		TableViewerColumn servicesColumn = SWTResourceManager.createTableViewerColumn(tableViewerService,	Messages.getString("mail_address"), 200);
+		TableViewerColumn servicesColumn = createTableViewerColumn("Service List",350);
 		servicesColumn.getColumn().setAlignment(SWT.LEFT);
 		servicesColumn.setLabelProvider(new ColumnLabelProvider() {
 			@Override
@@ -264,21 +277,30 @@ public class ServiceReportOnlineAhenkEditor  extends EditorPart{
 					
 					List<AgentServiceListItem> services=agentServices.getServices();
 					
-					StringBuilder servicesBuilder= new StringBuilder();
-					for (AgentServiceListItem agentServiceListItem : services) {
-						
-					}
-					
-					return services.toString();
+					return getServiceListStr(services);
 				}
 				return null;
 			}
 		});
-
-		
-		
 	}
 	
+	private String getServiceListStr(List<AgentServiceListItem> list) {
+		String serviceStr = "";
+		int i = 0;
+
+		for (AgentServiceListItem serv : list) {
+			i++;
+			String serviceName = serv.getServiceName() + " ("+ serv.getServiceStatus() + ") ";
+			String dddd = serviceName.replaceAll("\n", ",");
+			serviceStr += dddd + " - ";
+			if (i % 5 == 0)
+				serviceStr += "\n";
+		}
+		serviceStr += "\n";
+		String count = Messages.getString("table_column_service_count") + " : "+ new Integer(list.size()).toString();
+		serviceStr += count;
+		return serviceStr;
+	}
 	
 	private void getServices() {
 		agentList = XMPPClient.getInstance().getOnlineAgentPresenceMap();
@@ -287,6 +309,7 @@ public class ServiceReportOnlineAhenkEditor  extends EditorPart{
 			Boolean isOnline=agentList.get(agentDn);
 			if(isOnline){
 				try {
+					progressBar.setVisible(true);
 					
 					ArrayList<String> dns= new ArrayList<>();
 					dns.add(agentDn);
@@ -304,7 +327,6 @@ public class ServiceReportOnlineAhenkEditor  extends EditorPart{
 
 	@Override
 	public void setFocus() {
-		// TODO Auto-generated method stub
 		
 	}
 	
@@ -326,15 +348,13 @@ public class ServiceReportOnlineAhenkEditor  extends EditorPart{
 
 							@Override
 							public void run() {
-//								if (responseData != null && responseData.containsKey("ResultMessage")) {
-//									getServices();
-//								} else 
 									
 									if (responseData != null && ( responseData.containsKey("service_list") && responseData.containsKey("agent") )) {
 										
 										
 									String agentDn = (String) responseData.get("agent");
 									
+									@SuppressWarnings("unchecked")
 									List<HashMap<String, Object>> service_list = (List<HashMap<String, Object>>) responseData.get("service_list");
 									
 									Agent agent= new Agent();
@@ -351,16 +371,16 @@ public class ServiceReportOnlineAhenkEditor  extends EditorPart{
 										AgentServiceListItem item = new AgentServiceListItem();
 										item.setServiceName(map.get("serviceName").toString());
 										item.setServiceStatus(map.get("serviceStatus").toString());
-										// item.setStartAuto(map.get("startAuto").toString());
-										// item.setDesiredServiceStatus(DesiredStatus.NA);
-										// item.setDesiredStartAuto(DesiredStatus.NA);
 										services.add(item);
 									}
 									
 									agentServices.setServices(services);
 									agentServiceList.add(agentServices);
 									
+									if(agentList.size()==agentServiceList.size()){
 									tableViewerService.setInput(agentServiceList);
+									progressBar.setVisible(false);
+									}
 								}
 							}
 						});
@@ -379,7 +399,20 @@ public class ServiceReportOnlineAhenkEditor  extends EditorPart{
 			job.schedule();
 		}
 	};
+	private Button btnRefresh;
 
+	
+	private TableViewerColumn createTableViewerColumn(String title, int bound) {
+		final TableViewerColumn viewerColumn = new TableViewerColumn(
+				this.tableViewerService, SWT.FILL);
+		final TableColumn column = viewerColumn.getColumn();
+		column.setText(title);
+		column.setWidth(bound);
+		column.setResizable(true);
+		column.setMoveable(true);
+		column.setAlignment(SWT.FILL);
+		return viewerColumn;
+	}
 	
 	@Override
 	public void dispose() {
@@ -389,5 +422,92 @@ public class ServiceReportOnlineAhenkEditor  extends EditorPart{
 	}
 	
 
+	protected void filter(String agent, String serviceName, String status) {
 
+		if (agentServiceList != null) {
+
+			List<AgentServices> filteredList = new ArrayList<>();
+
+			for (AgentServices agentService : agentServiceList) {
+
+				AgentServices filteredService = new AgentServices();
+				
+				List<AgentServiceListItem> filteredServiceList = new ArrayList<AgentServiceListItem>();
+
+				if (agentService.getAgent().getDn().contains(agent))
+					for (AgentServiceListItem service : agentService.getServices()) {
+						
+						if (service.getServiceName().contains(serviceName)
+								&& ( status.equals("") || service.getServiceStatus().equals(status))){
+							filteredServiceList.add(service);
+						}
+
+					}
+
+				if (filteredServiceList.size() > 0) {
+
+					filteredService.setAgent(agentService.getAgent());
+
+					filteredService.setServices(filteredServiceList);
+
+					filteredList.add(filteredService);
+
+				}
+
+			}
+			tableViewerService.setInput(filteredList);
+			tableViewerService.refresh();
+
+		}
+
+	}
+	
+	protected void exportToPdf() {
+
+//		PdfExporter exporter = new PdfExporter(
+//				Messages.getString("service_report"));
+//
+//		exporter.addRow(Messages.getString("service_report"),
+//				PdfExporter.ALIGN_CENTER, exporter.getFont(
+//						PdfExporter.TIMES_ROMAN, 18, PdfExporter.BOLD,
+//						PdfExporter.RED));
+//		exporter.addEmptyLine(2);
+//
+//		String[] columnNames = { Messages.getString("table_column_agent"),
+//				Messages.getString("table_column_service_count") };
+//
+//		List<String[]> dataList = new ArrayList<>();
+//
+//		TableItem[] items = tableServiceList.getItems();
+//
+//		List<AgentServices> lstTableValue = new ArrayList<AgentServices>();
+//		for (int i = 0; i < items.length; i++) {
+//
+//			lstTableValue.add((AgentServices) items[i].getData());
+//		}
+//		for (AgentServices data : lstTableValue) {
+//
+//			String dn = data.getAgent().getDn();
+//			String col = "uid : "
+//					+ dn
+//					+ "\n"
+//					+ "cn : "
+//					+ LdapUtils.getInstance().findAttributeValueByDn(dn,
+//							"cn");
+//			dataList.add(new String[] { col,getServiceListStr(data.getServices()) });
+//		}
+//
+//		float[] columnWidths = { 1, 4 };
+//		exporter.addTable(columnWidths, columnNames, dataList);
+//		exporter.addRow(
+//				Messages.getString("report_date")
+//						+ ": "
+//						+ new SimpleDateFormat("dd/MM/yyyy hh:mm")
+//								.format(new java.util.Date()),
+//				PdfExporter.ALIGN_LEFT, exporter.getFont(PdfExporter.COURIER,
+//						8, PdfExporter.ITALIC, PdfExporter.BLUE));
+//		exporter.closeReport();
+
+	}
+	
 }
